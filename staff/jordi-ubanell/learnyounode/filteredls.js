365520e088b2ad6a3e9dd26b238fe1ea39f7a9bd
Jordi Ubanell <jordi@jordiubanell.com>
2018-08-07 09:55:13 +0200

    learnyounode inicial2

--- staff/jordi-ubanell/learnyounode/filteredls.js
+++ staff/jordi-ubanell/learnyounode/filteredls.js
@@ -1,16 +1,22 @@
 const fs = require('fs') 
+const path = require('path')
 
-function list(, 'txt'){
-
-    let result = fs.readdir('path', (process.argv[2]).toString())
+let result = fs.readdir(process.argv[2], fs.readdir(process.argv[3], list)
     
+function list() {
+    
+}
     console.log(list(result))
 }
 
 
-The fs.readdir() method takes a pathname as its first argument and a
-callback as its second. The callback signature is:
 
-   function callback (err, list) { /* ... */ }
 
-where list is an array of filename strings.
+// The fs.readdir() method takes a pathname as its first argument and a
+// callback as its second. The callback signature is:
+
+//    function callback (err, list) { /* ... */ }
+
+// where list is an array of filename strings.
+
+///////  fs.readdir(path[, options], callback)
